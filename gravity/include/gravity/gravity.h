d9c88be8e0ed020a3c43660c91708d771719ff8a
Hákon Hjaltalín <hakonmagnus45@gmail.com>
2022-04-29 23:22:50 +0000

    fix Windows MSVC options

---
+++ gravity/include/gravity/gravity.h
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2022 Hákon Hjaltalín
+//
+// This project is licensed under the MIT License. Please see LICENSE
+// or go to https://opensource.org/licenses/MIT for more information.
+//
+
+#pragma once
+
+#include "gravity/base/GrDefs.h"
+
